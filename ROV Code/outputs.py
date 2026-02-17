87ba86a483f511f8e70bbb2c6e0b5733b941d4ec
brayden-remington27 <156052485+brayden-remington27@users.noreply.github.com>
2026-02-17 17:01:39 +0300

    added some to output

--- ROV Code/outputs.py
+++ ROV Code/outputs.py
@@ -1,4 +1,9 @@
+import pigpio
+import sensors
+
 def init(config):
+
+    global PINS
     PINS = {
         "FRONT_LEFT_PIN": config.getint("THRUSTERS", "FRONT_LEFT_PIN", fallback=19),
         "FRONT_RIGHT_PIN": config.getint("THRUSTERS", "FRONT_RIGHT_PIN", fallback=16),
@@ -11,9 +16,33 @@ def init(config):
         "CAMERA_PIN": config.getint("GENERAL", "CAMERA_PIN", fallback=25),
         "LEAK_PIN": config.getint("GENERAL", "LEAK_PIN", fallback=5)
     }
+    global MAX_PW, MIN_PW, MID_PW
+    MAX_PW = config.getint("PWM", "MAX_PW", fallback=1900)
+    MIN_PW = config.getint("PWM", "MIN_PW", fallback=1100)
+    MID_PW = config.getint("PWM", "MID_PW", fallback=1500)
 
-    MAX_PW = config.getfloat("PWM", "MAX_PW", fallback=0.0019)
-    MIN_PW = config.getfloat("PWM", "MIN_PW", fallback=0.0011)
+
+
+    #global pi
+    #pi = pigpio.pi(config.get("NETWORKING", "PI_IP", fallback='raspberrypi.local'))
+    #assert pi.connected, "Pi not Connected"
+
+    print(config.get("NETWORKING", "PI_IP", fallback='raspberrypi.local'))
+
+    pi = pigpio.pi('10.42.0.91')
+    assert pi.connected, "pigpio not connected"   # local pigpiod
+
+    if not pi.connected:
+        raise RuntimeError("pigpio not connected")
+
+    if pi.connected:
+        sensors.setPiConnection(True)  # transmit pi status to sensors, to then be picked up by control then draw
+    else:
+        sensors.setPiConnection(False)
+
+
+def activationToPw(p):
+    return int(MID_PW + p * (MAX_PW - MID_PW))
 
 def sendActivations(activations: dict):
     pass  # TODO: actually send the activations to Tyrone
--- ROV Code/sensors.py
+++ ROV Code/sensors.py
@@ -1,7 +1,12 @@
-flags = {}
+flags = {
+    "piConnect": False
+}  # TODO: I also need to set up a flag to detect pi connection
 data = {
     "intTemp": 0,
     "extTemp": 0,
     "pressure": 0,
     "depth": 0
 }
+
+def setPiConnection(c: bool):
+    flags["piConnect"] = c
