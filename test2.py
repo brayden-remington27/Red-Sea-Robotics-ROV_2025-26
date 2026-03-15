b7ee0652cf2dd5f58d712cde76b8805f9199c41d
brayden-remington27 <156052485+brayden-remington27@users.noreply.github.com>
2026-03-15 10:43:42 +0300

    testing and new ip

--- test2.py
+++ test2.py
@@ -1,4 +1,27 @@
-import pigpio
-pi = pigpio.pi('10.42.0.91')
-pi.write(17, 0)
-print(pi.connected)
+# import pigpio
+# pi = pigpio.pi('10.42.0.91')
+# pi.write(17, 0)
+# print(pi.connected)
+
+######## TEST CAMERA BROADCAST FROM ROV ########
+
+import cv2
+
+# Initialize camera
+cap = cv2.VideoCapture("rtsp://10.42.0.91:8554/unicast")  # hope and pray that this is the right port to listen on
+
+while True:
+    # Read frame
+    ret, frame = cap.read()
+    if not ret: break
+
+    # Display frame
+    cv2.imshow('Live Feed', frame)
+
+    # Press 'q' to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resources
+cap.release()
+cv2.destroyAllWindows()
